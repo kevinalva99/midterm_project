85c07468a2291362039ae49c2dec4d7e4f139082
kevinalva99 <kevinalvarado2009@live.com>
2024-09-29 16:32:20 -0400

    method to collect the name and price of items as well as count how many times the item was ordered

--- midterm.py
+++ midterm.py
@@ -18,3 +18,18 @@ class ProcessedOrders():
 
     with open('customer.json', 'w') as customerFile:
       json.dump(customer, customerFile, indent=4)
+  
+  def itemCount(self):
+
+    item = {}
+
+    for items in self.data:
+      foodName = items["name"]
+      price = items['price']
+
+      if foodName not in item:
+        item[foodName] = {"price": price, "orders": 1}
+      else:
+        items[foodName]["orders"] += 1
+
+
