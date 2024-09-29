ce53d00d3a3b89b82728ee19bc0da88d20a76c6f
kevinalva99 <kevinalvarado2009@live.com>
2024-09-29 16:25:51 -0400

    method for collecting the names and phone numbers of customers

--- midterm.py
+++ midterm.py
@@ -4,3 +4,17 @@ class ProcessedOrders():
   def __init__(self, file):
     with open(file) as filename:
       self.data = json.load(filename)
+  
+  def customerInfo(self):
+
+    customer = {}
+
+    for information in self.data:
+      name = information["name"]
+      phoneNumber = information["phone"]
+
+      if phoneNumber not in customer:
+        customer[phoneNumber] = name
+
+    with open('customer.json', 'w') as customerFile:
+      json.dump(customer, customerFile, indent=4)
